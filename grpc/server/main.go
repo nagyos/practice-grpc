41cdb8b26bf16fe57ad10303a28569e9db4a2eab
shoki-tagawa <pupugshoki.wakaru.to.dekiru@gmail.com>
2023-01-03 13:27:20 +0900

    ✅ go-grpc-middleware/authによる認証の実装

--- grpc/server/main.go
+++ grpc/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"grpc/pb"
 	"io"
@@ -12,6 +13,8 @@ import (
 	"os"
 	"time"
 
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	"google.golang.org/grpc"
 )
 
@@ -143,13 +146,28 @@ func myLogging() grpc.UnaryServerInterceptor {
 	}
 }
 
+func authorize(ctx context.Context) (context.Context, error) {
+	token, err := grpc_auth.AuthFromMD(ctx, "Bearer")
+	if err != nil {
+		return nil, err
+	}
+
+	if token != "test-token" {
+		return nil, errors.New("bad token")
+	}
+	return ctx, nil
+}
+
 func main () {
 	lis, err := net.Listen("tcp", "localhost:50051")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer(grpc.UnaryInterceptor(myLogging()))
+	s := grpc.NewServer(grpc.UnaryInterceptor(
+		grpc_middleware.ChainUnaryServer(
+			myLogging(),
+			grpc_auth.UnaryServerInterceptor(authorize))))
 	pb.RegisterFileServiceServer(s, &server{})
 
 	fmt.Println("server is running...")
