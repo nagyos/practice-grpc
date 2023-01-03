9c3ba9ca55df4c0f52133b028d9137a7331f8c02
shoki-tagawa <pupugshoki.wakaru.to.dekiru@gmail.com>
2023-01-03 11:40:24 +0900

    ✅ クライアントストリーミングRPCの実装

--- grpc/client/main.go
+++ grpc/client/main.go
@@ -6,6 +6,8 @@ import (
 	"grpc/pb"
 	"io"
 	"log"
+	"os"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -19,7 +21,8 @@ func main () {
 
 	client := pb.NewFileServiceClient(conn)
 	// callListFiles(client)
-	callDownload(client)
+	// callDownload(client)
+	callUpload(client)
 }
 
 
@@ -53,4 +56,47 @@ func callDownload(client pb.FileServiceClient) {
 		log.Printf("Response from Download(string):%v", string(res.GetData()))
 		log.Println("asfd")
 	}
+}
+
+func callUpload(client pb.FileServiceClient) {
+	filename := "sports.txt"
+	path := "/home/shoki/dev/practice-grpc/grpc/storage/" + filename
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	stream, err := client.Upload(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	buf := make([]byte, 5)
+	for {
+		n, err := file.Read(buf)
+		if n == 0 || err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		req := &pb.UploadRequest{Data: buf[:n]}
+		sendErr := stream.Send(req)
+		if sendErr != nil {
+			log.Fatalln(err)
+		}
+
+		time.Sleep(1 * time.Second)
+
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("received data size: %v", res.GetSize())
 }
